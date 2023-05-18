7769bfbfea37ef8744d8e2cefbced756ceb8662f
mgckaled <marcelgckaled@gmail.com>
2023-05-18 17:05:11 -0300

    feat(server): upload stage 3 - trilha ignite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
-import fastify from 'fastify'
+import 'dotenv/config'
+
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import fastify from 'fastify'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -8,11 +12,18 @@ app.register(cors, {
   origin: true,
 })
 
+// secret: modo de diferenciação de tokens -  assinatura criptográfica
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3333')
