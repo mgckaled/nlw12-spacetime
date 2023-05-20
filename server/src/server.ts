fd48d0174e204e36d92974ae17822ffa52a4d235
mgckaled <marcelgckaled@gmail.com>
2023-05-20 19:53:43 -0300

    feat(server): upload stage 4 - trilha ignite

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,22 +2,32 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
 
-// secret: modo de diferenciação de tokens -  assinatura criptográfica
 app.register(jwt, {
   secret: 'spacetime',
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
