c6c1e70a81e8426a3adb045a421ab8bf8a014c10
mgckaled <marcelgckaled@gmail.com>
2023-05-20 19:28:28 -0300

    feat(mobile): upload stage 4 - trilha ignite

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,24 +1,11 @@
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from '@expo-google-fonts/roboto'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useRouter } from 'expo-router'
 import * as SecureStore from 'expo-secure-store'
-import { StatusBar } from 'expo-status-bar'
-import { styled } from 'nativewind'
 import { useEffect } from 'react'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-import { api } from '../src/lib/api'
-
-import blurBg from '../src/assets/bg-blur.png'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Stripes from '../src/assets/stripes.svg'
-
-const StyledStripes = styled(Stripes)
+import { api } from '../src/lib/api'
 
 export default function App() {
   const router = useRouter()
@@ -30,13 +17,7 @@ export default function App() {
       'https://github.com/settings/connections/applications/3257b33efbdf616d95ea',
   }
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
-  const [request, response, signInWithGithub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
       clientId: '3257b33efbdf616d95ea',
       scopes: ['identity'],
@@ -74,18 +55,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -113,8 +84,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.15.6:3333',
+  baseURL: 'http://192.168.1.104:3333',
 })
