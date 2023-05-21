7cd633361ea2938f427544fc480d0ce9bf60574b
mgckaled <marcelgckaled@gmail.com>
2023-05-21 14:59:49 -0300

    feat(web): upload stage 5 - trilha ignite

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -34,12 +34,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
         <main className="grid min-h-screen grid-cols-2">
-          {/* Left */}
           <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-            {/* Blur */}
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
-            {/* Stripes */}
             <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
             {isAuthenticated ? <Profile /> : <SignIn />}
@@ -47,8 +44,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <Copyright />
           </div>
 
-          {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -6,7 +6,7 @@ import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 export function Hero() {
   return (
     <div className="space-y-5">
-      <Image src={nlwLogo} alt="NLW Spacetime" />
+      <Image unoptimized src={nlwLogo} alt="NLW Spacetime" />
 
       <div className="max-w-[420px] space-y-1">
         <h1 className="text-5xl font-bold leading-tight text-gray-50">
